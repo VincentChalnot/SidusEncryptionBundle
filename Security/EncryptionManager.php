521e63f583680b8d4aae892e76daef9cde5802bb
Vincent Chalnot <vchalnot@clever-age.com>
2018-10-18 16:33:06 +0200

    Migrating to sodium: WIP

--- Security/EncryptionManager.php
+++ Security/EncryptionManager.php
@@ -487,6 +487,56 @@ class EncryptionManager
     }
 
     /**
+     * Wrap crypto_aead_*_encrypt() in a drop-dead-simple encryption interface
+     *
+     * @link https://paragonie.com/b/kIqqEWlp3VUOpRD7
+     *
+     * @param string $message
+     * @param string $key
+     *
+     * @return string
+     */
+    protected function encrypt(string $message, string $key): string
+    {
+        $nonce = random_bytes(24); // NONCE = Number to be used ONCE, for each message
+        $encrypted = sodium_crypto_aead_xchacha20poly1305_ietf_encrypt(
+            $message,
+            $nonce,
+            $nonce,
+            $key
+        );
+
+        return $nonce.$encrypted;
+    }
+
+    /**
+     * Wrap crypto_aead_*_decrypt() in a drop-dead-simple decryption interface
+     *
+     * @link https://paragonie.com/b/kIqqEWlp3VUOpRD7
+     *
+     * @param string $message - Encrypted message
+     * @param string $key     - Encryption key
+     *
+     * @return string
+     */
+    protected function decrypt(string $message, string $key): string
+    {
+        $nonce = mb_substr($message, 0, 24, '8bit');
+        $ciphertext = mb_substr($message, 24, null, '8bit');
+        $plaintext = sodium_crypto_aead_xchacha20poly1305_ietf_decrypt(
+            $ciphertext,
+            $nonce,
+            $nonce,
+            $key
+        );
+        if (!is_string($plaintext)) {
+            throw new Exception('Invalid message');
+        }
+
+        return $plaintext;
+    }
+
+    /**
      * @param string $name
      * @param string $category
      */
